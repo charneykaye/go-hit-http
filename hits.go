431e4ad482d575a3dfae12bca424f0725a72ba3a
Charney Kaye <charney@outright.io>
2016-02-02 17:06:55 -0500

    tests

--- hits.go
+++ hits.go
@@ -8,78 +8,127 @@ import (
 	"strings"
 	"math/rand"
 	"time"
+	"fmt"
+	"os"
 )
 
-const (
-	S_HITS = 100
-	S_METH = "POST"
-	S_HITTY_URL = "http://1cssvepl0m60.runscope.net/shittp"
-	S_PAYLOAD_LENGTH = 1024
+var (
+	HitTotal = 0
+	UseType = HitTypeB
+	UseMethod = "POST"
+	UseURL = "http://1cssvepl0m60.runscope.net/shittp"
+	UsePayloadLength = 1024
 )
 
 func main() {
-	qHits := make(chan Hit)
-	doneHits := make([]Hit, 0)
-	for i := 0; i < S_HITS; i++ {
-		s := Hit{HitTypeA, i, S_METH, S_HITTY_URL, randStringBytesMaskImpr(S_PAYLOAD_LENGTH), time.Now()}
-		go s.Hit(qHits)
+	setup()
+	done := make(chan Hit)
+	count := 0
+	for i := 0; i < HitTotal; i++ {
+		h := NewHit(UseType, i, UseMethod, UseURL, randStringBytesMaskImpr(UsePayloadLength))
+		log.WithFields(log.Fields{
+			"type": "a",
+			"num": h.num,
+		}).Info("Begin HTTP")
+		go h.Do(done)
 	}
-	for len(doneHits) < S_HITS {
-		h := <-qHits
-		doneHits = append(doneHits, h)
-	}
-	log.WithFields(log.Fields{
-		"total": len(doneHits),
-	}).Info("Done")
-}
-
-type Hit struct {
-	t testHitType
-	num int
-	meth string
-	url string
-	payload string
-	startAt time.Time
-}
-
-func (h *Hit) Hit(qHits chan<- Hit) {
-	switch h.t {
-		case HitTypeA:
+	for count < HitTotal {
+		h := <-done
+		if h.err != nil {
 			log.WithFields(log.Fields{
 				"type": "a",
 				"num": h.num,
-				}).Info("HTTP Request Begin")
-			client := &http.Client{}
-			req, _ := http.NewRequest(h.meth, h.url, strings.NewReader(h.payload))
-			req.Header.Add("Connection", "close")
-			req.Header.Add("Content-Type", "application/json")
-			resp, err := client.Do(req)
-			if err != nil {
-				log.WithFields(log.Fields{
-					"type": "a",
-					"num": h.num,
-					"error": err,
-					"duration": time.Since(h.startAt),
-					}).Warn("HTTP Request Failed")
-			} else {
-				io.Copy(ioutil.Discard, resp.Body)
-				resp.Body.Close()
-				log.WithFields(log.Fields{
-					"type": "a",
-					"num": h.num,
-					"duration": time.Since(h.startAt),
-					}).Info("HTTP Request OK")
-			}
-		case testHitTypeB:
-			// get f----d; there is no type B
+				"error": h.err,
+				"duration": time.Since(h.startAt),
+			}).Warn("Done HTTP (Failed)")
+		} else {
+			log.WithFields(log.Fields{
+				"type": "a",
+				"num": h.num,
+				"duration": time.Since(h.startAt),
+			}).Info("Done HTTP")
+		}
+		count++
 	}
-	qHits <- *h
+	teardown()
 }
 
-type testHitType string
+func setup() {
+	var err error
+	fmt.Printf("\nWelcome!\npid: %v\n\nEnter total # of HTTP to hit, then <ENTER>\n", os.Getpid())
+	_, err = fmt.Scanf("%d\n", &HitTotal)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\nEnter type of hit (A or B) then <ENTER>\n")
+	_, err = fmt.Scanf("%s\n", &UseType)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Okay, hitting %d times, type %s..\n\n", HitTotal, UseType)
+}
+
+func teardown() {
+	log.WithFields(log.Fields{
+		"total": HitTotal,
+	}).Info("Done")
+	var exit string
+	fmt.Printf("\nDone!\nWhen ready to exit pid %v, press <ENTER>\n", os.Getpid())
+	fmt.Scanf("%s\n", &exit)
+}
+
+func NewHit(t HitType, num int, meth string, url string, payload string) Hit {
+	return Hit {
+		t:t,
+		num:num,
+		meth:meth,
+		url:url,
+		payload:payload,
+		startAt: time.Now(),
+		err:nil,
+	}
+}
+
+type Hit struct {
+	t       HitType
+	num     int
+	meth    string
+	url     string
+	payload string
+	startAt time.Time
+	err     error
+}
+
+func (h *Hit) Do(done chan<- Hit) {
+	if h.t == HitTypeA {
+		client := &http.Client{}
+		req, _ := http.NewRequest(h.meth, h.url, strings.NewReader(h.payload))
+		req.Header.Add("Connection", "close")
+		req.Header.Add("Content-Type", "application/json")
+		_, err := client.Do(req)
+		if err != nil {
+			h.err = err
+		}
+	} else if h.t == HitTypeB {
+		client := &http.Client{}
+		req, _ := http.NewRequest(h.meth, h.url, strings.NewReader(h.payload))
+		req.Header.Add("Connection", "close")
+		req.Header.Add("Content-Type", "application/json")
+		resp, err := client.Do(req)
+		if err != nil {
+			h.err = err
+		} else {
+			io.Copy(ioutil.Discard, resp.Body)
+			resp.Body.Close()
+		}
+	}
+	done <- *h
+}
+
+type HitType string
 var (
-	HitTypeA = testHitType("A")
-	testHitTypeB = testHitType("B")
+	HitTypeA = HitType("A")
+	HitTypeB = HitType("B")
 )
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
